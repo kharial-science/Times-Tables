051521c2aabd92c2157bc605bf750ef72eebda16
Kharoh <Gaulu03@gmail.com>
2019-12-06 18:45:40 +0100

    Updates

---
+++ precache-manifest.38a727df70d5c822e659c81672a828fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0087134ac5d5154ae983398ce185fbe",
+    "url": "/Times-Tables/index.html"
+  },
+  {
+    "revision": "d1e80ae4a951695450b9",
+    "url": "/Times-Tables/static/css/main.6e957f04.chunk.css"
+  },
+  {
+    "revision": "f9602a88276d4615a4f1",
+    "url": "/Times-Tables/static/js/2.0cd9e0ee.chunk.js"
+  },
+  {
+    "revision": "d1e80ae4a951695450b9",
+    "url": "/Times-Tables/static/js/main.55a37270.chunk.js"
+  },
+  {
+    "revision": "0c67474ae07ad4e9abdf",
+    "url": "/Times-Tables/static/js/runtime-main.32edd96f.js"
+  }
+]);
